073f55d02a61044e76f8cbec20542a187bf17c5f
Jérémy <jeremy.bal97@gmail.com>
2022-12-17 13:20:41 +0100

    fix emission bug with Bloom effect :low_brightness:

--- src/Emissions.js
+++ src/Emissions.js
@@ -1,43 +1,54 @@
 import { useGLTF } from "@react-three/drei"
+import { useControls } from "leva"
 
 export default function Emissions()
 {
     const { nodes } = useGLTF('./emission1.glb')
-    
+    const scene = useGLTF('./emission1.glb')
+
+    const { color } = useControls({
+        color: {
+            value : [ 0.2, 2.4, 2.0 ],
+            min: 0,
+            max: 10,
+            step: 0.1
+        },
+    })
+
     return <>
         <group>
             <ambientLight color={'#ffffff'} />
 
             <mesh geometry={nodes.darkOrangeEmission.geometry}>
-                <meshStandardMaterial color={'#FF5836'} emissive={'#FF5836'} toneMapped={ false } emissiveIntensity={10} />
+                 <meshBasicMaterial color={ [ 1, 1, 5 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.blueEmission.geometry}>
-                <meshStandardMaterial color={'#99C9FF'} emissive={'#99C9FF'} toneMapped={ false } emissiveIntensity={1.2} />
+                 <meshBasicMaterial color={[color[0], color[1], color[2]]} toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.greenEmission.geometry}>
-                <meshStandardMaterial color={'#008F08'} emissive={'#008F08'} toneMapped={ false } emissiveIntensity={20} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.orangeEmission.geometry}>
-                <meshStandardMaterial color={'#FF9964'} emissive={'#FF9964'} toneMapped={ false } emissiveIntensity={2} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.pinkEmission.geometry}>
-                <meshStandardMaterial color={'#FC7BFF'} emissive={'#FC7BFF'} toneMapped={ false } emissiveIntensity={3} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.redEmission.geometry}>
-                <meshStandardMaterial color={'#B92223'} emissive={'#B92223'} toneMapped={ false } emissiveIntensity={10} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.whiteEmission.geometry}>
-                <meshStandardMaterial color={'#E3E3E3'} emissive={'#E3E3E3'} toneMapped={ false } emissiveIntensity={3} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
             
             <mesh geometry={nodes.yellowEmission.geometry}>
-                <meshStandardMaterial color={'#E1D22A'} emissive={'#E1D22A'} toneMapped={ false } emissiveIntensity={10} />
+                 <meshBasicMaterial color={ [ 1.5, 1, 4 ] } toneMapped={ false }/>
             </mesh>
         </group>
     </>
